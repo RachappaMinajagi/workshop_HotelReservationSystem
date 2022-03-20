903f2671be584cdeb5e05f4e0af8a9107cef201d
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-20 22:24:23 +0530

    Added: Add Hotel

---
+++ src/main/java/workShopHotelReservationSystem/App.java
@@ -0,0 +1,13 @@
+package workShopHotelReservationSystem;
+
+/**
+* Displaying WellCome Message  
+*
+*/
+public class App 
+{
+   public static void main( String[] args )
+   {
+       System.out.println( "WellCome TO Hotel Reservation Systeam" );
+   }
+}
---
+++ src/main/java/workShopHotelReservationSystem/HotelReservation.java
@@ -0,0 +1,24 @@
+package workShopHotelReservationSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Create Hotel Reservation Class
+ */
+public class HotelReservation {
+	 private List<Hotel> hotels;
+
+	    public HotelReservation() {
+	        this.hotels = new ArrayList<Hotel>();
+	    }
+
+	    public void add(Hotel hotel) {
+	        this.hotels.add(hotel);
+	    }
+
+	    public List<Hotel> getHotelList() {
+	        return this.hotels;
+	    }
+
+}
